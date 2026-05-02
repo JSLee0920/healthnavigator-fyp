35d3bab28d937a746fe09c0da4a3865ef4b9da39
Lee Jia Sheng <leejiasheng0826@gmail.com>
2026-05-02 09:00:28 +0800

    fix: use cookies

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,5 +1,4 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 import jwt
@@ -7,17 +6,20 @@ from app.core.config import settings
 from app.db.postgres_client import get_db
 from app.models.schema import User
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
-
-async def get_current_user(
-    token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)
-):
+async def get_current_user(request: Request, db: AsyncSession = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
     )
+
+    token = request.cookies.get("access_token")
+
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated. No access token cookie found.",
+        )
 
     try:
         payload = jwt.decode(
